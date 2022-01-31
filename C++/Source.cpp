0f63c2c054fe945c54055febe996e709744a6981
Dmytro <=>
2022-01-31 16:41:52 +0200

    int main

--- C++/Source.cpp
+++ C++/Source.cpp
@@ -16,23 +16,31 @@ public:
 class figure {
 	vector<point> v;
 public:
+	figure() {
+	}
 	figure(vector<point> v) {
 		this->v = v;
 	}
 	void set_figure(point p) {
 		v.push_back(p);
 	}
-
-
-
+	void print_figure() {
+		cout << string(40, '-')<<"\n";
+		for (auto it : v) { it.print(); }
+		cout << string(40, '-')<<"\n";
+	}
 };
 
 
 int main() {
-
-
-
-
+	vector<figure> vf;
+	figure f;
+	f.set_figure(point(4, 2));
+	f.set_figure(point(1, 8));
+	f.set_figure(point(5, -2));
+	f.set_figure(point(6, -12));
+	vf.push_back(f);
+	for (auto it : vf)it.print_figure();
 
 	return 0;
 }
