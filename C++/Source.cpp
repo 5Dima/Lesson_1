8ec61ff8c8f68e01a32c89bb163b1fd1d0c3779c
Dmytro <=>
2022-01-31 16:30:53 +0200

    class point

---
+++ C++/Source.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+
+using namespace std;
+class point {
+	int x, y;
+public:
+	point(int x, int y) { this->x = x; this->y = y;}
+	void print() {
+		cout << "X: " << x << "| Y: " << y << "\n";
+	}
+};
+
+
+
+
+int main() {
+
+
+
+
+
+	return 0;
+}
